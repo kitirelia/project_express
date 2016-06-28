6f32e2ec3ecc51f5e51b47ebb88241f9b05ab86e
aurelia <hs3prd@gmail.com>
2016-06-28 14:46:06 +0700

    done simple upload image

--- app.js
+++ app.js
@@ -1,30 +1,32 @@
 var express = require('express');
-//var birds = require('./routes/birds');
+var bodyParser = require('body-parser');
+//var multer = require('multer');
 var activity = require('./routes/activity');
 var upload = require('./routes/upload');
 var app = express();
+//var multer_upload = multer({ dest: 'uploads/' });
 
 //--- used ejs--
 app.set('view engine','ejs');
 
-//app.use('/birds', birds);
+//var upload = multer({ dest: 'uploads/' });
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 app.use('/activity',activity);
 app.use('/upload',upload);
 //--- home page
 app.get('/',function (req,res){
-	//res.send('Hello world'); //raw text
 	res.render('pages/index');
 });
-// about page 
-app.get('/about', function(req, res) {
-    res.render('pages/about');
-});
-// about page 
+
 app.get('/feed', function(req, res) {
     res.render('pages/feed');
 });
-
-
+// app.post('/upload', upload.single('avatar'),  function(req, res) {
+//   console.log(req.file);
+// });
 
 app.listen(3000,function (){
 	console.log('server start');
--- routes/upload.js
+++ routes/upload.js
@@ -1,8 +1,36 @@
 var express = require('express');
+var multer = require('multer');
+var path = require('path');
+var crypto = require('crypto');
 var router = express.Router();
+var storage = multer.diskStorage({
+  destination: './uploads/',
+  filename: function (req, file, cb) {
+    crypto.pseudoRandomBytes(16, function (err, raw) {
+      if (err) return cb(err)
 
-router.get('/',function (req,res){
-	 res.send('upload_page');
+      cb(null, raw.toString('hex') + path.extname(file.originalname))
+    })
+  }
+});
+
+var upload = multer({ storage: storage });
+// var upload = multer({dest:'./uploads/xxx',
+// 	rename: function(fieldname, filename) {
+//         return Date.now()+".jpg";
+//     }
+// });
+
+router.get('/', function(req, res) {
+  console.log('GET upload page');
+  res.send('Hello');
+}); 
+
+router.post('/', upload.single('avatar'),  function(req, res) {
+ 	console.log('file name  '+req.file.filename);
+ 	console.log('caption '+req.body.caption)
+  //console.log(req.file);
+	res.send('Successfully uploaded!');
 });
 
 module.exports = router;
