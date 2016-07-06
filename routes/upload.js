9ed57881dafeb79fbe1cd9b5e2677d416224ed26
aurelia <hs3prd@gmail.com>
2016-07-06 16:29:26 +0700

    done upload image, register via ajax

--- routes/upload.js
+++ routes/upload.js
@@ -2,8 +2,12 @@ var express = require('express');
 var multer = require('multer');
 var path = require('path');
 var crypto = require('crypto');
+var bodyParser = require('body-parser');
 var router = express.Router();
-var image_folder = "./uploads/"
+var image_folder = "./uploads/";
+
+
+
 var storage = multer.diskStorage({
   destination: image_folder,
   filename: function (req, file, cb) {
@@ -13,25 +17,51 @@ var storage = multer.diskStorage({
       cb(null, raw.toString('hex') + path.extname(file.originalname))
     })}
 });//end storage
+var file_filter;
+
+var upload = multer({ storage: storage }).single('this_image');
 
 
-var upload = multer({ storage: storage }).single('avatar');
-
+router.use(bodyParser.json()); // support json encoded bodies
+router.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 router.get('/', function(req, res) {
   console.log('GET upload page');
   res.send('Hello');
 }); 
 
 router.post('/',  function(req, res) {
+  //console.log('debug caption '+req.body.caption);
 	upload(req,res,function (err){
+    console.log("is files "+req.file, "caption "+req.caption);
+    console.log('---------');
+    console.log('track usernaem '+req.body.username);
+     console.log('track caption '+req.body.caption);
+    //console.log(req);
+    console.log('---------');
+    var result_str = "";
+    var msg="";
 		if(err){
-			return;
-		}
-		res.send('uploaded! '+req.file.filename);
+			return result_str="error";
+		}else{
+      if(req.file){
+        result_str="success";
+        msg='ok';
+      }
+      else{
+        result_str="error";
+        msg='no file'
+      }
+      var data = { 
+        result:result_str,
+        message:msg
+    }
+      res.json(data);
+      //res.json(aa);
+    //  console.log("req "+req.body.caption);
+    }
 	});
- 	//console.log('file name  '+req.file.filename);
- 	//console.log('caption '+req.body.caption)
-  //console.log(req.file);
+
+
 	
 });
 
